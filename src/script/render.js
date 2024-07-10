d5344c4b1be716e9ecf2905bb52e304b4ad0b1e8
xxIrynaxx <irakalachenko2006@gmail.com>
2024-07-10 11:10:28 +0300

    Список дел + localStorage - сохранение состояния

---
+++ src/script/render.js
@@ -0,0 +1,39 @@
+import { getItem, setItem } from './storage.js';
+
+const listElem = document.querySelector('.list');
+
+const compareTasks = (a, b) => {
+  return a.done - b.done || new Date(a.createDate) - new Date(b.createDate);
+};
+
+const createCheckbox = ({ done, id }) => {
+  const checkboxElem = document.createElement('input');
+  checkboxElem.setAttribute('type', 'checkbox');
+  checkboxElem.dataset.id = id;
+  checkboxElem.checked = done;
+  checkboxElem.classList.add('list__item-checkbox');
+
+  return checkboxElem;
+};
+
+const createListItem = ({ text, done, id }) => {
+  const listItemElem = document.createElement('li');
+  listItemElem.classList.add('list__item');
+  const checkboxElem = createCheckbox({ done, id });
+
+  if (done) {
+    listItemElem.classList.add('list__item_done');
+  }
+  listItemElem.append(checkboxElem, text);
+
+  return listItemElem;
+};
+
+export const renderTasks = () => {
+  const tasksList = getItem('tasksList') || [];
+
+  listElem.innerHTML = '';
+  const tasksElems = tasksList.sort(compareTasks).map(createListItem);
+
+  listElem.append(...tasksElems);
+};
